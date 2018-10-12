09d4db2d06b9a450a8fac8584eea808e920e8dec
sf314 <sf314@icloud.com>
2018-10-12 12:58:21 -0700

    Fixed import of new bashrc in setup script

--- setup_box.sh
+++ setup_box.sh
@@ -13,23 +13,25 @@ if [ $(which yum) ]; then
     echo "Using yum"
 fi 
 
-### 1: Programs 
+### 1: Programs: use provided PM to do so 
 echo "Installing common programs"
 $PMAN vim git gcc g++ make net-tools
 
 echo "Installing Core Flight System dependencies"
 $PMAN gcc-multilib g++-multilib python-qt4 pyqt4-dev-tools python-zmq
 
-### 2: bashrc 
+### 2: bashrc: source the version-controlled bashrc from the existing one
 echo "Configuring bashrc... "
 if [ -f $HOME/.bashrc ]; then
     echo "Modifying existing bashrc"
     echo "" >> $HOME/.bashrc
     echo "# Appended custom bashrc below:" >> $HOME/.bashrc
-    cat .bashrc >> $HOME/.bashrc
+    echo ". ~/common_dotfiles/.bashrc" >> $HOME/.bashrc
 else
     echo "No existing bashrc, creating new one"
-    cp .bashrc $HOME 
+    touch $HOME/.bashrc 
+    echo "# Appended custom bashrc below:" >> $HOME/.bashrc
+    echo ". ~/common_dotfiles/.bashrc" >> $HOME/.bashrc
 fi 
 
 ### 3: vimrc 
